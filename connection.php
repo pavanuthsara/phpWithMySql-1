6e3e9e5e50438218219b7823c0d92a1f3cc629f9
Pavan Uthsara <uthsarapavan@gmail.com>
2024-04-09 06:53:55 +0530

    successfully connected to the webserver

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php 
+
+	// Following command is used to connect to db server
+	// $connection = mysqli_connect(dbserver, dbuser, dbpass, dbname);
+
+	$connection = mysqli_connect('localhost', 'root', 'root', 'userdb');
+
+
+	// checking the connection
+	// mysqli_connect_errno();	mysqli_connect_error();
+	if(mysqli_connect_errno()){
+		die('Database connection failed '. mysqli_connect_error());
+	} else {
+		echo "Connection successful.";
+	}
+
+?>
